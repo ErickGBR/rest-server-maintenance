4a9ad7445a21728f375387c4a9e65ade58d64525
Erick <erickburgos1519@gmail.com>
2023-10-11 08:02:36 -0600

    first commit

---
+++ models/server.js
@@ -0,0 +1,60 @@
+// Import required packages and configure environment variables
+require("dotenv").config();
+const express = require('express');
+const cors = require('cors');
+const helmet = require('helmet');
+
+// Create a Server class
+class Server {
+    constructor() {
+        this.app = express(); // Initialize Express application
+        this.port = process.env.PORT; // Set the server port from environment variables
+        this.userRoutesPath = '/api/user'; // Define the base path for user routes
+        
+        // Middleware setup
+        this.middlewares();
+        
+        // Error handling setup
+        this.errorHandling();
+        
+        // Define routes
+        this.routes();
+    }
+
+    // Middleware configuration
+    middlewares() {
+        // Enable Cross-Origin Resource Sharing (CORS)
+        this.app.use(cors());
+        
+        // Parse incoming JSON requests
+        this.app.use(express.json());
+        this.app.use(helmet());
+        
+        // Serve static files from the 'public' directory
+        this.app.use(express.static('public'));
+    }
+
+    // Define routes
+    routes() {
+        // Use the user routes defined in the 'user' module
+        this.app.use(this.userRoutesPath, require('../routes/user'));
+    }
+
+    // Error handling middleware
+    errorHandling() {
+        this.app.use((err, req, res, next) => {
+            console.error(err.stack);
+            res.status(500).send('Something broke!');
+        });
+    }
+
+    // Start the server and listen on the defined port
+    listen() {
+        this.app.listen(this.port, () => {
+            console.log("Running server on port:", this.port);
+        });
+    }
+}
+
+// Export the Server class for use in other modules
+module.exports = Server;
