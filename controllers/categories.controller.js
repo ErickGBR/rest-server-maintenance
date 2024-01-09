0ee40cb9472f7fb94fbe81820dbf9a99313c3e21
Erick Burgos <43017724+ErickGBR@users.noreply.github.com>
2024-01-09 08:41:23 -0600

    add crud of categories

--- controllers/categories.controller.js
+++ controllers/categories.controller.js
@@ -1,5 +1,6 @@
 const { response } = require("express");
 const { Categories } = require("../models");
+
 const createCategories = (req, res = response) => {
   const name = req.body.name.toUpperCase();
   const categoriesDb = Categories.findOne({ name });
@@ -8,18 +9,55 @@ const createCategories = (req, res = response) => {
       msg: `This category ${name} already exists`,
     });
   }
-
   const data = {
     name,
     user: req.user._id,
   };
-
   const category = new Categories(data);
   category.save();
-
   return res.status(201).json(category);
 };
 
+
+const getCategories = async (req, res = response) => {
+  let { limit = 5, page = 0 } = req.query;
+  //create pagination and populate
+  const [total, categories] = await Promise.all([
+    Categories.countDocuments({ status: true }),
+    Categories.find({ status: true })
+      .limit(limit)
+      .skip(page)
+      .populate("user", "name"),
+  ]);
+  res.status(200).json({
+    data: categories,
+    total,
+  });
+};
+
+
+const updateCategories = async (req, res = response) => {
+  const { id } = req.params;
+  const { name } = req.body;
+  const category = await Categories.findByIdAndUpdate(id, { name });
+  res.status(200).json({
+    category,
+  });
+};
+
+
+const deleteCategories = async (req, res = response) => {
+  const { id } = req.params;
+  const category = await Categories.findByIdAndUpdate(id, { status: false });
+  res.status(200).json({
+    category,
+  });
+}
+
+
 module.exports = {
   createCategories,
+  getCategories,
+  updateCategories,
+  deleteCategories
 };
