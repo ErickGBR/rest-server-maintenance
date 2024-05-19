d831ec94c809eb9f4833ebd490747197d6b96535
ErickGBR <erickburgos1519@gmail.com>
2024-05-19 16:49:56 -0600

    feat: file upload helper

---
+++ helpers/upload-file.js
@@ -0,0 +1,24 @@
+
+
+const uploadFile = (files, validExtensions = ['jpg', 'jpeg', 'png', 'gif'], folder = "") => {
+
+  return new Promise((resolve, reject) => {
+    // The name of the input field (i.e. "file") is used to retrieve the uploaded file
+    const { file } = files;
+    const extension = file.name.split(".")[1];
+
+    if (!validExtensions.includes(extension)) {
+      reject(`Extention file is not valid - ${extension}`)
+    }
+    const tempName = `${uuidv4()}.${extension}`;
+    const uploadPath = path.join(__dirname, '../uploads/' + folder + tempName)
+    // Use the mv() method to place the file somewhere on your server
+    file.mv(uploadPath, function (err) {
+      if (err)
+        reject(err)
+    });
+    resolve(uploadPath)
+
+  })
+
+}
