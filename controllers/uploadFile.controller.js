a40353d954aaf5286242ee243cba76613f802ce6
ErickGBR <erickburgos1519@gmail.com>
2024-05-28 23:37:21 -0600

    feat: validate public id

--- controllers/uploadFile.controller.js
+++ controllers/uploadFile.controller.js
@@ -100,6 +100,12 @@ const updateImageClouddinary = async (req, res = response) => {
   }
 
 
+  if(model.img){
+    const nameArr = model.img.split("/")
+    const name = nameArr[nameArr.length - 1]
+    const [public_id]= name.split(".")
+  }
+
   //clean previus images
   const { tempFilePath } = req.files.file
   const { secure_url } = await cloudinary.uploader.upload(tempFilePath)
